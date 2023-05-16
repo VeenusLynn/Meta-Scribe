11f19602050603110fff83892aa75a2f603ffed0
Lyna NOURA <l.noura@esi-sba.dz>
2023-05-16 17:43:32 +0100

    search for champion by name intead of id

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def get_champion_rotation():
     except requests.exceptions.RequestException as err:
         print(f"Error occurred: {err}")
 
-def get_champion_info(champion_id):
+
+def get_champion_info(champion_name):
     url = 'https://ddragon.leagueoflegends.com/cdn/13.9.1/data/en_US/champion.json'
 
     try:
@@ -34,6 +35,24 @@ def get_champion_info(champion_id):
         for champ in champions.values():
             if int(champ['key']) == champion_id:
                 return champ
+
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except requests.exceptions.RequestException as err:
+        print(f"Error occurred: {err}")
+
+def get_champion_id(champion_name):
+    url = 'https://ddragon.leagueoflegends.com/cdn/13.9.1/data/en_US/champion.json'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        champion_data = response.json()
+
+        champions = champion_data['data']
+        for champ in champions.values():
+            if champ['name'].lower() == champion_name.lower():
+                return int(champ['key'])
 
     except requests.exceptions.HTTPError as err:
         print(f"HTTP error occurred: {err}")
@@ -63,12 +82,13 @@ if choice == '1':
     else:
         print("Failed to fetch champion rotation.")
 elif choice == '2':
-    # Prompt the user for a champion ID
-    champion_id = int(input("Enter a champion ID: "))
+    # Prompt the user for a champion name
+    champion_name = input("Enter a champion name: ")
+    champion_id = get_champion_id(champion_name) 
 
     champion_info = get_champion_info(champion_id)
     if champion_info:
-        print(f"Champion ID: {champion_id}")
+        print(f"Champion ID: {champion_info['key']}")
         print(f"Name: {champion_info['name']}")
         print(f"Title: {champion_info['title']}")
         print(f"Blurb: {champion_info['blurb']}")
