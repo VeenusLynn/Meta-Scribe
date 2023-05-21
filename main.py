288f8654b71778b70166f1e5b43dbe1191779d48
not1cyyy <firas1120@gmail.com>
2023-05-21 20:32:01 +0100

    Added tier function

--- main.py
+++ main.py
@@ -35,6 +35,40 @@ def get_version():
 VERSION = get_version()#
 ########################
 
+def get_champion_tier(champion_name):
+    url = f'https://u.gg/lol/champions/{champion_name}/build'
+
+    # Set custom headers to mimic a web browser
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+
+    # Send an HTTP GET request to the u.gg Irelia build page with custom headers
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find the elements containing the tier
+        tier_element = soup.find('div', {'class': 'champion-tier media-query media-query_MOBILE_LARGE__DESKTOP_LARGE'})
+
+        # Extract the text from the elements
+        tier = tier_element.text.strip()
+
+        if tier_element:
+            # Extract the text from the element and add a space after the percentage sign
+            tier = tier_element.text.strip().replace('Tier', '')
+
+            return tier
+
+    else:
+        # Request was unsuccessful
+        print(f"Failed to retrieve winrate. Status code: {response.status_code}")
+        return None
+ 
+
 def get_champion_winrate(champion_name):
     url = f'https://u.gg/lol/champions/{champion_name}/build'
 
@@ -574,11 +608,13 @@ def main():
 
     elif choice == '8':
         champion_name = input("Enter a champion name: ")
+        champion_tier = get_champion_tier(champion_name)
         champion_winrate = get_champion_winrate(champion_name)
         champion_pickrate = get_champion_pickrate(champion_name)
         champion_banrate = get_champion_banrate(champion_name)
 
         if champion_winrate:
+            print(f"Tier: {champion_tier}")
             print(f"Win rate: {champion_winrate}")
             print(f"Pick rate: {champion_pickrate}")
             print(f"Ban rate: {champion_banrate}")
