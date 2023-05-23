cb56026762d304fb407c9a4c35220ca9b6bbe4c8
not1cyyy <firas1120@gmail.com>
2023-05-23 19:58:45 +0100

    Added shit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import dotenv
 import os
+import interactions
 from bs4 import BeautifulSoup
 
 from utils import *
@@ -12,21 +13,21 @@ dotenv.load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 # Create an instance of the bot
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
-bot.remove_command('help')
+bot = interactions.Client(token=os.getenv("DISCORD_TOKEN"))
+
 
 
 @bot.event
 async def on_ready():
-    print(f'Bot is ready. Logged in as {bot.user.name} ({bot.user.id})')
+    print(f'Bot is ready.')
+    await bot.change_presence(interactions.ClientPresence(activities=[interactions.PresenceActivity(name=f"League Of Legends", type=interactions.PresenceActivityType.WATCHING)]))
 
 
 @bot.command(
     name="championrotation",
     description="See the current champion rotation"
-)
-async def champion_rotation(ctx):
+    )
+async def champion_rotation(ctx: interactions.CommandContext):
 
     try:
         await ctx.send("Please wait while we fetch the champion rotation...")
@@ -49,9 +50,17 @@ async def champion_rotation(ctx):
 
 @bot.command(
     name="championinfo",
-    description="Get information about a specific champion"
+    description="Get information about a specific champion",
+        options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def champion_info(ctx, champion_name: str):
+async def champion_info(ctx: interactions.CommandContext, champion_name: str):
 
     try:
         champ = print_champion_info_by_name(champion_name)
@@ -86,9 +95,17 @@ async def champion_info(ctx, champion_name: str):
 
 @bot.command(
     name="iteminfo",
-    description="Get information about a specific item"
+    description="Get information about a specific item",
+        options = [
+        interactions.Option(
+            name="item_name",
+            description="Which item do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def item_info(ctx, item_name: str):
+async def item_info(ctx: interactions.CommandContext, item_name: str):
 
     try:
         item = print_item_info_by_name(item_name)
@@ -110,9 +127,17 @@ async def item_info(ctx, item_name: str):
 
 @bot.command(
     name="runeinfo",
-    description="Get information about a specific rune"
+    description="Get information about a specific rune",
+        options = [
+        interactions.Option(
+            name="rune_name",
+            description="Which rune do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def rune_info(ctx, rune_name: str):
+async def rune_info(ctx: interactions.CommandContext, rune_name: str):
 
     try:
         rune = print_rune_info_by_name(rune_name)
@@ -131,9 +156,17 @@ async def rune_info(ctx, rune_name: str):
 
 @bot.command(
     name="summonerspellinfo",
-    description="Get information about a specific summoner spell"
+    description="Get information about a specific summoner spell",
+        options = [
+        interactions.Option(
+            name="spell_name",
+            description="Which summoner spell do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def summoner_spell_info(ctx, spell_name: str):
+async def summoner_spell_info(ctx: interactions.CommandContext, spell_name: str):
 
     try:
         spell = print_summoner_spell_info_by_name(spell_name)
@@ -152,26 +185,48 @@ async def summoner_spell_info(ctx, spell_name: str):
 
 @bot.command(
     name="championsplashart",
-    description="Get a champion's splash art"
+    description="Get a champion's splash art",
+        options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def champion_splash_art(ctx, champion_name: str):
+async def champion_splash_art(ctx: interactions.CommandContext, champion_name: str):
 
     try:
         splash_art_url = print_champion_splash_art(champion_name)
-        # load image from url and send it to the channel
+
         await ctx.send(splash_art_url)
     except:
         await ctx.send("Champion not found.")
 
 @bot.command(
     name="championskinsplashart",
-    description="Get a champion's skin splash art"
+    description="Get a champion's skin splash art",
+        options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True
+        ),
+        interactions.Option(
+            name="skin_number",
+            description="Which skin do you want to get information about?",
+            type=interactions.OptionType.INTEGER,
+            required=True
+        )
+    ]
 )
-async def champion_skin_splash_art(ctx, champion_name: str, skin_number: int):
+async def champion_skin_splash_art(ctx: interactions.CommandContext, champion_name: str, skin_number: int):
 
     try:
         splash_art_url = print_champion_skin_splash_art(champion_name, skin_number)
-        # load image from url and send it to the channel
+
         await ctx.send(splash_art_url)
     
     except:
@@ -179,9 +234,17 @@ async def champion_skin_splash_art(ctx, champion_name: str, skin_number: int):
 
 @bot.command(
     name="championstats",
-    description="Get a champion's current stats"
+    description="Get a champion's current stats",
+        options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def champion_stats(ctx, champion_name: str):
+async def champion_stats(ctx: interactions.CommandContext, champion_name: str):
     
         try:
             await ctx.send("Please wait while we fetch the champion stats...")
@@ -193,7 +256,7 @@ async def champion_stats(ctx, champion_name: str):
 
 
             stats = f"""
-**Current stats for {champion_name}**:
+**Current stats for {champion_name.capitalize()}**:
 **Tier**: {tier}
 **Winrate**: {winrate}
 **Pickrate**: {pickrate}
@@ -207,15 +270,23 @@ async def champion_stats(ctx, champion_name: str):
 
 @bot.command(
     name="championcounters",
-    description="Get a champion's counters"
+    description="Get a champion's counters",
+        options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def champion_counters(ctx, champion_name: str):
+async def champion_counters(ctx: interactions.CommandContext, champion_name: str):
     
     try:
         await ctx.send("Please wait while we fetch the champion counters...")
         best_picks, worst_picks = print_champion_counters(champion_name)
         message = f"""
-\r**Best Picks vs {champion_name}**:
+\r**Best Picks vs {champion_name.capitalize()}**:
         """
         for i in best_picks:
             message += f"\r**Champion Name**: {i['champion_name']}"
@@ -224,7 +295,7 @@ async def champion_counters(ctx, champion_name: str):
             message += f"\n"
         
         message += f"""
-\r**Worst Picks vs {champion_name}**:
+\r**Worst Picks vs {champion_name.capitalize()}**:
         """
         for i in worst_picks:
             message += f"\r**Champion Name**: {i['champion_name']}"
@@ -242,15 +313,23 @@ async def champion_counters(ctx, champion_name: str):
 
 @bot.command(
     name="championrunes",
-    description="Get a champion's recommended runes"
+    description="Get a champion's recommended runes",
+        options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def champion_recommended_runes(ctx, champion_name: str):
+async def champion_recommended_runes(ctx: interactions.CommandContext, champion_name: str):
     
     try:
         await ctx.send("Please wait while we fetch the champion runes...")
         primary_tree_name, primary_runes, secondary_tree_name, secondary_runes, stat_shards = print_champion_recommended_runes(champion_name)
         message = f"""
-\r**Recommended runes for {champion_name}**:
+\r**Recommended runes for {champion_name.capitalize()}**:
 \r**Primary Tree**: {primary_tree_name}
 \r**Primary Runes**:
         """
@@ -278,15 +357,23 @@ async def champion_recommended_runes(ctx, champion_name: str):
 
 @bot.command(
     name="championspells",
-    description="Get a champion's recommended spells"
+    description="Get a champion's recommended spells",
+        options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def champion_recommended_spells(ctx, champion_name: str):
+async def champion_recommended_spells(ctx: interactions.CommandContext, champion_name: str):
 
     try:
         await ctx.send("Please wait while we fetch the champion spells...")
         spells = print_champion_recommended_spells(champion_name)
         message = f"""
-\r**Recommended spells for {champion_name}**:
+\r**Recommended spells for {champion_name.capitalize()}**:
         """
         for spell in spells:
             message += f"\r{spell.replace('Summoner Spell', '').strip()}"
@@ -298,15 +385,23 @@ async def champion_recommended_spells(ctx, champion_name: str):
 
 @bot.command(
     name="championbuild",
-    description="Get a champion's recommended build"
+    description="Get a champion's recommended build",
+            options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def champion_recommended_build(ctx, champion_name: str):
+async def champion_recommended_build(ctx: interactions.CommandContext, champion_name: str):
     
     try: 
         await ctx.send("Please wait while we fetch the champion build...")
         starter_items, core_build, boots, final_build = print_champion_recommended_build(champion_name)
         message = f"""
-\r**Recommended build for {champion_name}**:
+\r**Recommended build for {champion_name.capitalize()}**:
 \r**Starter items**:
         """
         for item in starter_items:
@@ -339,15 +434,23 @@ async def champion_recommended_build(ctx, champion_name: str):
 
 @bot.command(
     name="championskillorder",
-    description="Get a champion's recommended skill order"
+    description="Get a champion's recommended skill order",
+        options = [
+        interactions.Option(
+            name="champion_name",
+            description="Which champion do you want to get information about?",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ]
 )
-async def champion_skill_order(ctx, champion_name: str):
+async def champion_skill_order(ctx: interactions.CommandContext, champion_name: str):
     
     try:
         await ctx.send("Please wait while we fetch the champion skill order...")
         skill_order = print_champion_skill_order(champion_name)
         message = f"""
-\r**Recommended skill order for {champion_name}**:
+\r**Recommended skill order for {champion_name.capitalize()}**:
         """
         for skill in skill_order:
             message += f"\r**Ability**: {skill['skill_label']}"
@@ -364,25 +467,25 @@ async def champion_skill_order(ctx, champion_name: str):
     name="help",
     description="Show available commands"
 )
-async def help_command(ctx):
+async def help_command(ctx: interactions.CommandContext):
     help_text = """
 Available commands:
-- !championrotation: See current champion rotation
-- !championinfo <champion_name>: Get information about a specific champion
-- !iteminfo <item_name>: Get information about a specific item
-- !runeinfo <rune_name>: Get information about a specific rune
-- !summonerspellinfo <spell_name>: Get information about a specific summoner spell
-- !championsplashart <champion_name>: Get a champion's splash art
-- !championskinsplashart <champion_name> <skin_number>: Get a champion's skin splash art
-- !championstats <champion_name>: Get a champion's current stats
-- !championcounters <champion_name>: Get a champion's counters
-- !championrecommendedrunes <champion_name>: Get a champion's recommended runes
-- !championrecommendedspells <champion_name>: Get a champion's recommended spells
-- !championrecommendedbuild <champion_name>: Get a champion's recommended build
-- !championskillorder <champion_name>: Get a champion's recommended skill order
+- /championrotation: See current champion rotation
+- /championinfo <champion_name>: Get information about a specific champion
+- /iteminfo <item_name>: Get information about a specific item
+- /runeinfo <rune_name>: Get information about a specific rune
+- /summonerspellinfo <spell_name>: Get information about a specific summoner spell
+- /championsplashart <champion_name>: Get a champion's splash art
+- /championskinsplashart <champion_name> <skin_number>: Get a champion's skin splash art
+- /championstats <champion_name>: Get a champion's current stats
+- /championcounters <champion_name>: Get a champion's counters
+- /championrecommendedrunes <champion_name>: Get a champion's recommended runes
+- /championrecommendedspells <champion_name>: Get a champion's recommended spells
+- /championrecommendedbuild <champion_name>: Get a champion's recommended build
+- /championskillorder <champion_name>: Get a champion's recommended skill order
     """
 
     await ctx.send(help_text)
 
 if __name__ == '__main__':
-    bot.run(os.getenv("DISCORD_TOKEN"))
+    bot.start()
